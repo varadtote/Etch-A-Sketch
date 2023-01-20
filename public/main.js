3639fba0858ea0aeccb78e2c5c2e0f10b554b9de
Varad Tote <varadtote1111@gmail.com>
2023-01-20 15:04:44 +0530

    onhover change color effect added

--- public/main.js
+++ public/main.js
@@ -4,17 +4,20 @@ boxes.addEventListener('click', () => {
     boxes = prompt("No of Boxes (0-100)");
     document.getElementById('out').innerHTML = boxes;
     MultipleCaller();
+    // sketch();
 })
 
 function Divadders() {
     let sketch = document.querySelector('.sketch-container');
     sketch.style.gridTemplateColumns = `repeat(${boxes}, 1fr)`;
     let newParentDiv = document.createElement('div');
-    newParentDiv.setAttribute('div', 'inside');
+
+    newParentDiv.setAttribute('class', 'grids');
     newParentDiv.style.backgroundColor = 'white';
     newParentDiv.style.border = "1px solid black";
     newParentDiv.style.width = "100%";
     newParentDiv.style.height = "100%";
+    const cssTemplateString = ".grids:[hover]{backgroundColor: red}";
     sketch.appendChild(newParentDiv);
 }
 
@@ -23,5 +26,13 @@ function MultipleCaller() {
     for (i = 0; i < boxes * boxes; i++) {
         Divadders();
     }
-
 }
+
+
+// function sketch() {
+//     let box = document.querySelector('.grids');
+//     box.addEventListener('mouseover', (event) => {
+//         event.target.style.backgroundColor = "red";
+//     })
+// }
+
